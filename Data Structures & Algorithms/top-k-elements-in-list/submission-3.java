1ff7bc384a3ef6ff250c96f909d6cb3ea9e5c359
Vaishali Sontakke <vaishali.sontakke1@gmail.com>
2026-04-04 02:26:50 +0530

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> hmap1 = new HashMap<>();
+        for(int i=0; i< nums.length ; i++){
+                hmap1.put(nums[i], hmap1.getOrDefault(nums[i], 0)+1);
+        }
+
+        // priority Queue with descending order
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) ->
+        hmap1.get(b) - hmap1.get(a));
+        // add all keys to priority queue, order based on above
+        pq.addAll(hmap1.keySet());
+
+        int[] topK = new int[k];
+        for(int i=0; i< k ; i++){
+            topK[i] = pq.poll();
+        }
+
+        return topK;
+
+    }
+}
