42bf7a437d6126e5eac6e4e185af50824c416f27
Vaishali Sontakke <vaishali.sontakke1@gmail.com>
2026-04-05 23:39:23 +0530

    Add: three-integer-sum - submission-14

---
+++ Data Structures & Algorithms/three-integer-sum/submission-14.java
@@ -0,0 +1,54 @@
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        // edge conditions
+        if (nums.length < 3){
+            return result;
+        }
+        if(nums.length == 3){
+            if(nums[0] + nums[1] + nums[2] == 0){
+                ArrayList<Integer> curr = new ArrayList<>(Arrays.asList(nums[0], nums[1], nums[2]));
+                result.add(curr);    
+            }
+            return result;
+        }  
+        // sort the array for two pointer solution.
+        Arrays.sort(nums);
+        for(int i=0; i < nums.length ; i++){
+            // duplicates skip
+            if(i!=0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int l=i+1, r = nums.length-1;
+            while(l < r){
+                int tempSum = nums[i] + nums[l] + nums[r];
+                if(tempSum == 0){
+                    ArrayList<Integer> curr = new ArrayList<>(Arrays.asList(nums[i], nums[l], nums[r]));
+                    if(!result.contains(curr)){
+                        result.add(curr);
+                    }
+                    l++; r--;
+                    while(nums[l] == nums[l-1] && l<r){
+                        l++;
+                    }
+                    while(nums[r] == nums[r+1] && l<r){
+                        r--;
+                    }
+                } else if(tempSum < 0){
+                    l++;
+                    while(nums[l] == nums[l-1] && l<r){
+                        l++;
+                    }
+                } else {
+                    r--;
+                    while(nums[r] == nums[r+1] && l<r){
+                        r--;
+                    }
+                }
+                
+            }
+        }
+       return result; 
+    }
+}
