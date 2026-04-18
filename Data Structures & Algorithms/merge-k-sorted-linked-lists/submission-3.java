cac178d0efa560c2c63c4299c64692fd398a0736
Vaishali Sontakke <vaishali.sontakke1@gmail.com>
2026-04-18 20:52:05 +0530

    Add: merge-k-sorted-linked-lists - submission-3

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-3.java
@@ -0,0 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// Recursive merge is faster. use divide and conquer approach like binary search.
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0){
+            return null;
+        }
+        //return mergeRangeLists(lists, 0, lists.length-1);
+        for(int i=1; i< lists.length; i++){
+            lists[0] = mergeTwoLists(lists[0], lists[i]);
+        }
+        return lists[0];
+    }
+
+    public ListNode mergeRangeLists(ListNode[] lists, int start, int end){
+        if(start == end){
+            return lists[start];
+        }
+        if(end-start == 1){
+            return mergeTwoListsRecursive(lists[start], lists[end]);
+        }
+        int mid = (start + end)/2;
+        ListNode left = mergeRangeLists(lists, start, mid);
+        ListNode right = mergeRangeLists(lists, mid+1, end);
+        return mergeTwoListsRecursive(left, right);
+    }
+
+    public ListNode mergeTwoListsRecursive(ListNode l1, ListNode l2){
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+        if(l1.val < l2.val){
+            l1.next = mergeTwoListsRecursive(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoListsRecursive(l1, l2.next);
+            return l2;
+        }
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
+        ListNode l1 = list1, l2= list2;
+        ListNode node1= new ListNode(0); // dummy node
+        ListNode res = node1;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                node1.next = l1;
+                node1 = node1.next;
+                l1 = l1.next;
+            } else {
+                node1.next = l2;
+                node1 = node1.next;
+                l2 = l2.next;
+            }
+        }
+        if(l1 != null){
+            node1.next = l1;
+        }
+        if(l2 != null){
+            node1.next = l2;
+        }
+        return res.next;
+    }
+}
