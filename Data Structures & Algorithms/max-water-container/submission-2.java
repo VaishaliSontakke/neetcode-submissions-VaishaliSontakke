3d3d6e6bc2fb86dfa8dd65f82b865ed3afa0899b
Vaishali Sontakke <vaishali.sontakke1@gmail.com>
2026-04-20 15:24:02 +0530

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+   public int maxArea(int[] heights) {
+        // two pointer technique. find curr area and update max.
+        // move the lesser height pointer.
+        int l=0, r = heights.length - 1;
+        int maxArea = Integer.MIN_VALUE;
+        while(l<r){
+            int tempArea = (r-l) * Math.min(heights[l], heights[r]);
+            maxArea = Math.max(tempArea, maxArea);
+            if(heights[l] <= heights[r]){
+                l++;
+            }else{
+                r--;
+            }
+        }
+        return maxArea;
+        
+    }
+}
