1928cc416f97aef75c9d62aa5e8c43b409b43da1
Vaishali Sontakke <vaishali.sontakke1@gmail.com>
2026-04-20 15:38:02 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int l =0;
+        int profit=0;
+        for(int r=0; r <prices.length; r++){
+            int currProfit = prices[r] - prices[l];
+            profit = Math.max(profit, currProfit);
+            while(currProfit < 0 && (l<r)){
+                l++;
+                currProfit = prices[r] - prices[l];
+                continue;
+            }
+            
+        }
+        return profit;
+        
+    }
+}
