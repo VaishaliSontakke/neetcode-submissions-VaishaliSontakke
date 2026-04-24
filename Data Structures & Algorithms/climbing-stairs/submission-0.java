567c0b7ec14d487486048d572664c189c62377c1
Vaishali Sontakke <vaishali.sontakke1@gmail.com>
2026-04-24 12:56:02 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        int prev2 = 1,  prev=2, curr =0;
+        for(int i=3; i<=n;i++){
+            curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return curr;
+        
+    }
+}
